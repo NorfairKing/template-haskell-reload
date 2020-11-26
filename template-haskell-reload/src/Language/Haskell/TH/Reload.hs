297bccc2fb1db0bd7d4b0d48543f883b5cc78772
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-11-26 22:26:33 +0100

    Loading entire directories

--- template-haskell-reload/src/Language/Haskell/TH/Reload.hs
+++ template-haskell-reload/src/Language/Haskell/TH/Reload.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DeriveLift #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Language.Haskell.TH.Reload
@@ -18,17 +19,34 @@ module Language.Haskell.TH.Reload
     embedReadTextFileLiveRun,
     embedReadTextFileBakedIn,
     embedReadTextFileBakedInRun,
+
+    -- ** List directory
+    embedListDir,
+    embedListDirLive,
+    embedListDirLiveRun,
+    embedListDirBakedIn,
+    embedListDirBakedInRun,
+
+    -- ** Files in directory
+    embedTextFilesIn,
+    embedTextFilesInWith,
   )
 where
 
+import Conduit
+import Control.Monad
 import Control.Monad.IO.Class
 import qualified Data.ByteString as SB
+import Data.List
+import Data.Map (Map)
+import qualified Data.Map as M
 import Data.Text (Text)
 import qualified Data.Text.Encoding as TE
 import GHC.Generics (Generic)
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
 import Path
+import Path.IO
 
 type Load a = LoadT IO a
 
@@ -75,6 +93,7 @@ loadMode = \case
   Live _ -> LoadLive
   BakedIn _ -> BakeIn
 
+-- | Embed a text file, this will throw on a non-utf8 files
 embedReadTextFile :: Mode -> Path Rel File -> Q (TExp (Load Text))
 embedReadTextFile = \case
   LoadLive -> embedReadTextFileLive
@@ -93,8 +112,99 @@ embedReadTextFileBakedIn fp = do
 
 embedReadTextFileBakedInRun :: Path Rel File -> Q Text
 embedReadTextFileBakedInRun fp = do
-  runIO $ putStrLn $ unwords ["Adding file", fromRelFile fp]
+  runIO $ putStrLn $ unwords ["Baking in file:", fromRelFile fp]
   qAddDependentFile (fromRelFile fp)
   contents <- runIO (SB.readFile (fromRelFile fp))
   let textContents = TE.decodeUtf8 contents
   pure textContents
+
+-- | Embed a directory, ignore hidden files and directories
+embedListDir :: Mode -> Path Rel Dir -> Q (TExp (Load [Path Rel File]))
+embedListDir = \case
+  LoadLive -> embedListDirLive
+  BakeIn -> embedListDirBakedIn
+
+embedListDirBakedIn :: Path Rel Dir -> Q (TExp (Load [Path Rel File]))
+embedListDirBakedIn rd = do
+  runIO $ putStrLn $ unwords ["Baking in directory:", fromRelDir rd]
+  cts <- embedListDirBakedInRun rd
+  [||BakedIn cts||]
+
+embedListDirBakedInRun :: Path Rel Dir -> Q [Path Rel File]
+embedListDirBakedInRun rd = runIO $ Conduit.sourceToList $ sourceFilesInNonHiddenDirsRecursively rd
+
+embedListDirLive :: Path Rel Dir -> Q (TExp (Load [Path Rel File]))
+embedListDirLive rd = [||Live $ embedListDirLiveRun rd||]
+
+embedListDirLiveRun :: Path Rel Dir -> IO [Path Rel File]
+embedListDirLiveRun rd = Conduit.sourceToList $ sourceFilesInNonHiddenDirsRecursively rd
+
+sourceFilesInNonHiddenDirsRecursively ::
+  forall m i.
+  MonadIO m =>
+  Path Rel Dir ->
+  ConduitT i (Path Rel File) m ()
+sourceFilesInNonHiddenDirsRecursively rd = do
+  here <- getCurrentDir
+  walkDirRel go $ here </> rd
+  where
+    go ::
+      Path Rel Dir ->
+      [Path Rel Dir] ->
+      [Path Rel File] ->
+      ConduitT i (Path Rel File) m (WalkAction Rel)
+    go curdir subdirs files = do
+      Conduit.yieldMany $
+        map (curdir </>) $
+          filter (not . hidden) files
+      pure $ WalkExclude $ filter (isHiddenIn curdir) subdirs
+
+embedTextFilesIn ::
+  Mode ->
+  Path Rel Dir ->
+  Q (TExp (Load (Map (Path Rel File) Text)))
+embedTextFilesIn = embedTextFilesInWith id [||id||] (flip const) [||flip const||]
+
+embedTextFilesInWith ::
+  (Ord a, Lift a, Lift b) =>
+  (Path Rel File -> a) ->
+  Q (TExp (Path Rel File -> a)) ->
+  (a -> Text -> b) ->
+  Q (TExp (a -> Text -> b)) ->
+  Mode ->
+  Path Rel Dir ->
+  Q (TExp (Load (Map a b)))
+embedTextFilesInWith keyFunc qKeyFunc valFunc qValFunc heated rd = case heated of
+  LoadLive ->
+    [||
+    Live $ do
+      files <- embedListDirLiveRun rd
+      tups <- forM files $ \file -> do
+        let key = $$(qKeyFunc) file
+        val <- $$(qValFunc) key <$> embedReadTextFileLiveRun (rd </> file)
+        pure (key, val)
+      pure $ M.fromList tups
+    ||]
+  BakeIn -> do
+    files <- embedListDirBakedInRun rd
+    tups <- forM files $ \file -> do
+      let key = keyFunc file
+      val <- valFunc key <$> embedReadTextFileBakedInRun (rd </> file)
+      pure (key, val)
+    [||BakedIn $ M.fromList tups||]
+
+hidden :: Path Rel File -> Bool
+hidden = goFile
+  where
+    goFile :: Path Rel File -> Bool
+    goFile f = isHiddenIn (parent f) f
+    goDir :: Path Rel Dir -> Bool
+    goDir f
+      | parent f == f = False
+      | otherwise = isHiddenIn (parent f) f || goDir (parent f)
+
+isHiddenIn :: Path b Dir -> Path b t -> Bool
+isHiddenIn curdir ad =
+  case stripProperPrefix curdir ad of
+    Nothing -> False
+    Just rp -> "." `isPrefixOf` toFilePath rp
--- template-haskell-reload/test/Language/Haskell/TH/ReloadSpec.hs
+++ template-haskell-reload/test/Language/Haskell/TH/ReloadSpec.hs
@@ -4,6 +4,8 @@
 
 module Language.Haskell.TH.ReloadSpec (spec) where
 
+import Data.Map (Map)
+import qualified Data.Map as M
 import Data.Text (Text)
 import qualified Data.Text as T
 import Language.Haskell.TH.Reload
@@ -16,6 +18,18 @@ thisFileBakedIn = $$(embedReadTextFile BakeIn [relfile|test/Language/Haskell/TH/
 thisFileLive :: Load Text
 thisFileLive = $$(embedReadTextFile LoadLive [relfile|test/Language/Haskell/TH/ReloadSpec.hs|])
 
+testDirBakedIn :: Load [Path Rel File]
+testDirBakedIn = $$(embedListDir BakeIn [reldir|test|])
+
+testDirLive :: Load [Path Rel File]
+testDirLive = $$(embedListDir LoadLive [reldir|test|])
+
+testFilesBakedIn :: Load (Map (Path Rel File) Text)
+testFilesBakedIn = $$(embedTextFilesIn BakeIn [reldir|test|])
+
+testFilesLive :: Load (Map (Path Rel File) Text)
+testFilesLive = $$(embedTextFilesIn LoadLive [reldir|test|])
+
 spec :: Spec
 spec = do
   describe "embedReadTextFile" $ do
@@ -23,11 +37,37 @@ spec = do
       it "is baked in" $ do
         loadMode thisFileBakedIn `shouldBe` BakeIn
       it "contains the module name" $ do
-        contents <- loadIO thisFileBakedIn
+        contents <- load thisFileBakedIn
         T.unpack contents `shouldContain` "Language.Haskell.TH.ReloadSpec"
     describe "Live" $ do
       it "is baked in" $ do
         loadMode thisFileLive `shouldBe` LoadLive
       it "contains the module name" $ do
-        contents <- loadIO thisFileLive
+        contents <- load thisFileLive
         T.unpack contents `shouldContain` "Language.Haskell.TH.ReloadSpec"
+  describe "embedListDir" $ do
+    describe "Baked in" $ do
+      it "is baked in" $ do
+        loadMode testDirBakedIn `shouldBe` BakeIn
+      it "contains this module" $ do
+        files <- load testDirBakedIn
+        files `shouldContain` [[relfile|Language/Haskell/TH/ReloadSpec.hs|]]
+    describe "Live" $ do
+      it "is live" $ do
+        loadMode testDirLive `shouldBe` LoadLive
+      it "contains this module" $ do
+        files <- load testDirLive
+        files `shouldContain` [[relfile|Language/Haskell/TH/ReloadSpec.hs|]]
+  describe "embedTextFilesIn" $ do
+    describe "Baked in" $ do
+      it "is baked in" $ do
+        loadMode testFilesBakedIn `shouldBe` BakeIn
+      it "contains this module" $ do
+        files <- load testFilesBakedIn
+        files `shouldSatisfy` (M.member [relfile|Language/Haskell/TH/ReloadSpec.hs|])
+    describe "Live" $ do
+      it "is live" $ do
+        loadMode testFilesLive `shouldBe` LoadLive
+      it "contains this module" $ do
+        files <- load testFilesLive
+        files `shouldSatisfy` (M.member [relfile|Language/Haskell/TH/ReloadSpec.hs|])
